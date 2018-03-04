ae3ffba36c62a601ba23afb17cbb6160555b9913
robert hosking <roberthosking96@gmail.com@>
2018-03-03 21:54:01 -0500

    tabnavigation added

--- App.js
+++ App.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import { Text, View } from 'react-native';
+import { TabNavigator } from 'react-navigation'; // Version can be specified in package.json
+import { Platform } from 'react-native'
+
 import ShopView from './components/ShopView'
 import MasterView from './components/MasterView'
 
+const tabBarOptions = Platform.OS === 'ios' ?
+  {
+    // iOS tabBarOptions
+    showLabel: true
+  } : {
+    // Android tabBarOptions
+    showIcon: true,
+    showLabel: true
+  }
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   render() {
     return (
-      <ShopView />
-      // <MasterView />
-    )
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
   }
 }
+
+class RecipesScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
+
+class ShopListScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
+
+export default TabNavigator({
+  Recipes: {
+    screen: RecipesScreen
+
+  },
+
+  Home: { screen: HomeScreen },
+  Shop: {screen: ShopListScreen}
+},
+{
+  tabBarPosition: 'bottom',
+}
+);
